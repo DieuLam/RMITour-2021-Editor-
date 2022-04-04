7261a556ae4d88022139c355cf803d502abc2ad5
Thuanlengoc2511 <s3877961@rmit.edu.vn>
2022-04-04 15:50:27 +0700

    distance

---
+++ Assets/Script/CalculatePath.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculatePath : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Destination;
+
+    [SerializeField]
+    private Text DistanceText;
+
+    private float distance;
+
+    private void Update()
+    {
+        distance = (Destination.transform.position - transform.position).magnitude;
+
+        DistanceText.text = "Distance: " + distance.ToString("F1") + " m";
+    }
+}
