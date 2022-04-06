3863f09e2274df75cb43004e6c9403f9fc3c2678
Thuanlengoc2511 <s3877961@rmit.edu.vn>
2022-04-06 11:42:07 +0700

    coordinate

---
+++ Assets/Script/UpdateGPSText.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour
+{
+    public Text coordinates;
+
+    private void Update()
+    {
+        coordinates.text = "lat: " + GPS.Instance.latitude.ToString() + " Lon:" + GPS.Instance.longitude.ToString();
+    }
+}
